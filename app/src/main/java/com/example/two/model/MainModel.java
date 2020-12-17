dceaa03c716d22f421ab2b4933dcb91eff5bbe3b
ch1024 <2744566936@qq.com>
2020-12-17 20:09:35 +0800

    创建mvp类

---
+++ app/src/main/java/com/example/two/model/MainModel.java
@@ -0,0 +1,11 @@
+package com.example.two.model;
+
+import com.example.two.contract.Contract;
+
+public class MainModel implements Contract.Model {
+
+    @Override
+    public void getBean() {
+
+    }
+}
---
+++ app/src/main/java/com/example/two/presenter/MainPresenter.java
@@ -0,0 +1,10 @@
+package com.example.two.presenter;
+
+import com.example.two.contract.Contract;
+
+public class MainPresenter implements Contract.Presenter {
+    @Override
+    public void getBean() {
+
+    }
+}
